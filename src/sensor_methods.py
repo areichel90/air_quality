9c492079000d1de2fafa7fc5466418db7557ff2b
Andrew Reichel <andrewreichel@andrews-mbp.mynetworksettings.com>
2024-12-07 20:14:19 -0500

    changing from uart to i2c

--- src/sensor_methods.py
+++ src/sensor_methods.py
@@ -1,12 +1,12 @@
 import board, busio
-from adafruit_pm25.uart import PM25_UART
+from adafruit_pm25.i2c import PM25_I2C
 
 
 class Sensor:
     def __init__(self):
-        uart = busio.UART(board.TX, board.RX, baudrate=9600)
+        uart = busio.I2C(board.SCL, board.SDA, frequency=100000)
         reset_pin = None
-        self.pm25 = PM25_UART(uart, reset_pin)
+        self.pm25 = PM25_I2C(uart, reset_pin)
 
 if __name__ == "__main__":
     print("script complete.")
