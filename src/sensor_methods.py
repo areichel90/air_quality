7c5b3484c60af51b498d74bff5b000c3addba782
Andrew Reichel <andrewreichel@andrews-mbp.mynetworksettings.com>
2024-12-07 20:08:27 -0500

    simple sensor class w/ rpi init method

--- src/sensor_methods.py
+++ src/sensor_methods.py
@@ -1,4 +1,12 @@
 import board, busio
+from adafruit_pm25.uart import PM25_UART
+
+
+class Sensor:
+    def __init__(self):
+        uart = busio.UART(board.TX, board.RX, baudrate=9600)
+        reset_pin = None
+        self.pm25 = PM25_UART(uart, reset_pin)
 
 if __name__ == "__main__":
-    print("complete")
+    print("script complete.")
